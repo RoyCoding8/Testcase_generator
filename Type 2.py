e47529c2d5d946cfd0290d4f6ce0bd3a0ccd70eb
RoyCoding8 <royshashwata8@gmail.com>
2022-12-15 09:22:12 +0530

    Update Type 2.py

--- Type 2.py
+++ Type 2.py
@@ -2,20 +2,29 @@
 Format:
     t
     n
+
     a1 a2 ... an
 """
 
+
 import random as rn
 
-ln=int(input("Enter lower bound for t: "))
-un=int(input("Enter upper bound for t: "))
-lm=int(input("Enter lower bound for n: "))
-um=int(input("Enter upper bound for n: "))
+lt=int(input("Enter lower bound for t: "))
+ut=int(input("Enter upper bound for t: "))
+
+ln=int(input("Enter lower bound for n: "))
+un=int(input("Enter upper bound for n: "))
+
 la=int(input("Enter lower bound for ai: "))
 ua=int(input("Enter upper bound for ai: "))
-t=rn.randrange(ln,un+1)
-n=rn.randrange(lm,um+1)
+
+t=rn.randrange(lt,ut+1)
+
+n=rn.randrange(ln,un+1)
+
 a=rn.sample(range(la,ua+1),n)
+
 for _ in range(t):
     print(n)
+
     print(*a)
